f0f82132ef85566cf3715e4053641ba8ffff27fd
jangerhard <jgschoepp@gmail.com>
2014-02-13 15:00:24 +0100

    Endret på theme

--- ProtoType1/src/com/bbv/prototype1/Kalkulator.java
+++ ProtoType1/src/com/bbv/prototype1/Kalkulator.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
