5aea1dd75cdd3c1c6f61a17bf58bacf52869bd46
AniEye <laugaland_6@hotmail.com>
2014-02-18 12:12:56 +0100

    trying to find out how to view a file

--- ProtoType1/src/com/bbv/prototype1/FileView.java
+++ ProtoType1/src/com/bbv/prototype1/FileView.java
@@ -1,5 +1,27 @@
 package com.bbv.prototype1;
 
-public class FileView {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FileView extends Activity {
+
+	TextView _head,_content;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fileview);
+		Initializa();
+	}
+	private void Initializa() {
+		// TODO Auto-generated method stub
+		_head = (TextView)findViewById(R.id.tvFileOrigin);
+		_content = (TextView)findViewById(R.id.tvFileView);
+	}
+	public void setTextViews(){
+		String _filename = "test.txt";
+		_head.setText(_filename);
+	}
 
 }
