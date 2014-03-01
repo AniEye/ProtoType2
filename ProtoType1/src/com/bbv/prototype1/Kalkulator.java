76dbedd734fad37efa1c79a46f5aa53fbb89b8b2
jangerhard <jgschoepp@gmail.com>
2014-03-01 18:57:04 +0100

    Shows same calculator when switching orientation
    However this is persistant even after closing the app

--- ProtoType1/src/com/bbv/prototype1/Kalkulator.java
+++ ProtoType1/src/com/bbv/prototype1/Kalkulator.java
@@ -1,7 +1,8 @@
 package com.bbv.prototype1;
 
-import com.bbv.prototype1.Kalkulatorer.*;
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,8 +15,13 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
+import com.bbv.prototype1.Kalkulatorer.*;
 
-public class Kalkulator extends Activity implements OnItemSelectedListener, OnItemClickListener{
+public class Kalkulator extends Activity implements OnItemSelectedListener,
+		OnItemClickListener {
+
+	String lastChoiceMadeKey = "lastChoice";
+	int lastChoice;
 
 	ListView valg_horizontal;
 	Spinner valg_vertical;
@@ -28,6 +34,9 @@ public class Kalkulator extends Activity implements OnItemSelectedListener, OnIt
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+
+		lastChoice = loadLC(); //Loads the last chosen calculator
+
 		setContentView(R.layout.activity_calculator);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		initialize();
@@ -63,6 +72,8 @@ public class Kalkulator extends Activity implements OnItemSelectedListener, OnIt
 
 			lLayout = (LinearLayout) findViewById(R.id.lInSVKalkVertical);
 			valg_vertical.setOnItemSelectedListener(this);
+			valg_vertical.setSelection(lastChoice); // Chooses which item the
+													// spinner starts at
 		} else {
 			valg_horizontal = (ListView) findViewById(R.id.listKalkHorizontal);
 
@@ -76,26 +87,29 @@ public class Kalkulator extends Activity implements OnItemSelectedListener, OnIt
 
 			lLayout = (LinearLayout) findViewById(R.id.lInSVKalkHorizontal);
 			valg_horizontal.setOnItemClickListener(this);
-			showCalc(0);
+			showCalc(lastChoice); // Chooses which item the list starts at
 		}
 	}
-
 
 	@Override
 	public void onItemSelected(AdapterView<?> arg0, View arg1, int index,
 			long arg3) {
 
 		showCalc(index);
-		
+
 	}
-	
+
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int index, long arg3) {
 		// TODO Auto-generated method stub
 		showCalc(index);
 	}
-	
-	public void showCalc(int index){
+
+	public void showCalc(int index) {
+		/**
+		 * Shows the calculator selected in either the spinner or the listview
+		 */
+		
 		if (lLayout.getChildCount() > 0)
 			lLayout.removeAllViews();
 
@@ -157,11 +171,35 @@ public class Kalkulator extends Activity implements OnItemSelectedListener, OnIt
 			Log.println(Log.ERROR, "TryCatch",
 					"Somehow no calculator was selected!");
 		}
+
+		saveLC(index); //Saves the current index here
+	}
+
+	private int loadLC() {
+		/**
+		 * Loads previous choice in either the spinner or the listview
+		 */
+		SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+		int restoredInt = prefs.getInt(lastChoiceMadeKey, 0);
+
+		return restoredInt;
+
+	}
+
+	private void saveLC(int index) {
+		/**
+		 * Saves the current position of the selected item in either the spinner 
+		 * or the listview
+		 */
+		SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+		editor.putInt(lastChoiceMadeKey, index);
+		editor.commit();
 	}
 
 	@Override
 	public void onNothingSelected(AdapterView<?> arg0) {
 		// TODO Auto-generated method stub
 	}
+
 
 }
