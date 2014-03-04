4d2e16a7177150a84442df2315595b64b504075b
AniEye <laugaland_6@hotmail.com>
2014-03-04 17:38:50 +0100

    rendering a page from a standard text file
    made the program able to read and make a page from the content of a text
    file that is standard for every chapter and chapterpart, have added this
    standard text file to each chapter as well

--- ProtoType1/src/com/bbv/prototype1/Fragment_1.java
+++ ProtoType1/src/com/bbv/prototype1/Fragment_1.java
@@ -8,55 +8,72 @@ import java.io.InputStreamReader;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.LinearGradient;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.webkit.WebView.FindListener;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class Fragment_1 extends Fragment implements OnClickListener {
 
 	protected FrameLayout flTextContent;
-	protected TextView _title;
 	protected Button _Next, _Last;
 	protected View rootView;
 	protected String _Chapter, _ChapterPart1, _ChapterPart2;
 	protected LinearLayout _LinLay;
+	protected ScrollView _Scroll;
+	protected int _ViewHeight, _ViewWidth;
+	protected Bundle currentBundle,nextBundle,lastBundle;
+	protected AssetManager as;
 
+	// make sure it can read special characters like זרו
+	//example of solution is unicodereader
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		rootView = inflater.inflate(R.layout.fragment_1, container, false);
 		Initialize();
+		try{
+		currentBundle = getActivity().getIntent().getExtras();
+		as = getActivity().getAssets();
+		}catch(Exception e){
+			Log.println(Log.ERROR, "FileView", "Didn't get bundle");
+		}
+		decodeDocument();
+		return rootView;
+	}
+	protected void decodeDocument(){
 		try {
-			Bundle getBundle = getActivity().getIntent().getExtras();
-			String filename = "pros_og_teori_text/" + makeFileName(getBundle);
+			
+			String filename = makeTeoriFileName(currentBundle) + "_.txt";
 			String str = "";
-			// String filename = "pros_og_teori_text/"
 			try {
 
+				
 				Log.println(Log.ERROR, "FileView", "Did get bundle");
-				AssetManager as = getActivity().getAssets();
+				
 				InputStream is = as.open(filename);
 				BufferedReader reader = new BufferedReader(
 						new InputStreamReader(is));
-				String aText = "";
 
 				Boolean _bTitle = false;
 				Boolean _bText = false;
 				Boolean _bImage = false;
-				int x=0,y=0;
+				Boolean _bNextAndLast = false;
+				Boolean _bLast = false;
+
 				StringBuffer buffer = new StringBuffer();
 				if (is != null) {
 					while ((str = reader.readLine()) != null) {
@@ -68,7 +85,12 @@ public class Fragment_1 extends Fragment implements OnClickListener {
 
 						} else if (str.contains("</title")) {
 							_bTitle = false;
-							_title.setText(buffer.toString());
+							TextView addTitle = new TextView(getActivity());
+							addTitle.setText(buffer.toString());
+							addTitle.setTextColor(Color.RED);
+							addTitle.setGravity(Gravity.CENTER);
+							addTitle.setTextSize(30);
+							_LinLay.addView(addTitle);
 							continue;
 
 						} else if (str.contains("<text>")) {
@@ -84,20 +106,52 @@ public class Fragment_1 extends Fragment implements OnClickListener {
 									"The buffer reads" + buffer);
 							_LinLay.addView(addText);
 							continue;
-						}else if(str.contains("<image>")){
+						} else if (str.contains("<image>")) {
 							_bImage = true;
+							buffer = new StringBuffer();
 							continue;
-						}else if(str.contains("</image>")){
-							_bImage=false;
-							ImageView image = new ImageView(getActivity());
-							image.setMaxHeight(y);
-							image.setMaxWidth(x);
-//							image.set//have to get the picture from the assets folder
+						} else if (str.contains("</image>")) {
+							_bImage = false;
+							try {
+								String _imageFilename = makeTeoriFileName(currentBundle)
+										+ buffer.toString();
+
+								InputStream iis = getActivity().getAssets()
+										.open(_imageFilename);
+								ImageView image = new ImageView(getActivity());
+								image.setPadding(0, 10, 0, 10);
+								Bitmap thePicture = BitmapFactory
+										.decodeStream(iis);
+								image.setImageBitmap(thePicture);
+								_LinLay.addView(image);
+							} catch (Exception e) {
+								Log.println(Log.ERROR, "Sizeing",
+										"The picture didn't open");
+							}
+							continue;
+						} else if (str.contains("<next>")||str.contains("<last>")) {
+							_bNextAndLast = true;
+							buffer = new StringBuffer();
+							continue;
+						} else if (str.contains("</next>")) {
+							_bNextAndLast = false;
+							nextBundle = createNewBundle(buffer.toString());
+							continue;
+						}else if(str.contains("</last>")){
+							_bNextAndLast = false;
+							lastBundle = createNewBundle(buffer.toString());
+							continue;
 						}
-						if (_bTitle || _bText) {
+
+						if (_bText) {
 							buffer.append(str);
-							if (str.isEmpty())
-								buffer.append("\n");
+							buffer.append("\n");
+						} else if (_bImage) {
+							buffer.append(str);
+						} else if (_bTitle) {
+							buffer.append(str);
+						}else if(_bNextAndLast){
+							buffer.append(str);
 						}
 					}
 				}
@@ -109,42 +163,52 @@ public class Fragment_1 extends Fragment implements OnClickListener {
 		} catch (Exception e) {
 			Log.println(Log.ERROR, "FileView", "Didn't get bundle");
 		}
-		return rootView;
 	}
 
 	protected void Initialize() {
 		_Next = (Button) rootView.findViewById(R.id.bNext);
 		_Next.setOnClickListener(this);
 		_Last = (Button) rootView.findViewById(R.id.bLast);
-		_Last.setOnClickListener(this);
-		// flTextContent = (FrameLayout) rootView
-		// .findViewById(R.id.flText_Content);
-		_title = (TextView) rootView.findViewById(R.id.tvText_Title);
+		_Last.setOnClickListener(this);	
 		_LinLay = (LinearLayout) rootView.findViewById(R.id.llFrag);
-		// Fragment newFragment = new Fragment_2();
-		// FragmentManager fm = getFragmentManager();
+	}
 
+	public Bundle createNewBundle(String filePath) {
+		Bundle newBundle = new Bundle();
+		filePath =filePath.trim();
+		String[] filePathDevided = filePath.split("/");
+		if (filePathDevided.length > 1) {
+			newBundle.putString("Teori_Chapter", filePathDevided[0]);
+			newBundle.putString("Teori_ChapterPart1", filePathDevided[1]);
+			newBundle.putString("Teori_ChapterPart2", null);
+			if (filePathDevided.length > 2) {
+				newBundle.putString("Teori_ChapterPart2", filePathDevided[2]);
+			}
+		}
+return newBundle;
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.bNext:
-			// Fragment newFragment = new Fragment_2();
-			// FragmentManager fm = getFragmentManager();
-			// fm.beginTransaction().replace(R.id.flText_Content,
-			// newFragment).commit();
+			_LinLay.removeAllViews();
+			currentBundle=nextBundle;
+			decodeDocument();
 			break;
 		case R.id.bLast:
+			_LinLay.removeAllViews();
+			currentBundle=lastBundle;
+			decodeDocument();
 			break;
 		}
 
 	}
 
-	private String makeFileName(Bundle theBundle) {
-		String filename = "";
+	private String makeTeoriFileName(Bundle theBundle) {
+		String filename = "pros_og_teori_text/";
 		if (theBundle.getString("Teori_Chapter") != null) {
-			filename = theBundle.getString("Teori_Chapter") + "/";
+			filename = filename + theBundle.getString("Teori_Chapter") + "/";
 			if (theBundle.getString("Teori_ChapterPart1") != null) {
 				filename = filename + theBundle.getString("Teori_ChapterPart1")
 						+ "/";
@@ -153,7 +217,7 @@ public class Fragment_1 extends Fragment implements OnClickListener {
 							+ theBundle.getString("Teori_ChapterPart2") + "/";
 			}
 		}
-		filename = filename + "_.txt";
+
 		return filename;
 	}
 
